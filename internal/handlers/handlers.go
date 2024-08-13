a5fc59a5e9bc40b011c3388eab71cca31cf81450
netzen86 <netzen0su@gmail.com>
2024-08-13 23:49:18 +0500

    modify Update handler, added handler for Retrieve metrics

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -13,17 +13,20 @@ import (
 func UpdateMHandle(storage repositories.Repo) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.Background()
-		mValue := chi.URLParam(r, "mValue")
 		mType := chi.URLParam(r, "mType")
+		if mType != "counter" && mType != "gauge" {
+			http.Error(w, "wrong metric type", http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 		mName := chi.URLParam(r, "mName")
-
+		mValue := chi.URLParam(r, "mValue")
 		err := storage.UpdateParam(ctx, mType, mName, mValue)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-
 		w.WriteHeader(http.StatusOK)
 		fmt.Println(storage)
 	}
@@ -32,9 +35,41 @@ func UpdateMHandle(storage repositories.Repo) http.HandlerFunc {
 func RetrieveMHandle(storage repositories.Repo) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.Background()
-		t, _ := template.ParseFiles("../../web/template/metrics.html") //setp 1
+		t, _ := template.ParseFiles("../../web/template/metrics.html")
 		t.Execute(w, storage.GetMemStorage(ctx))
-		// t.Execute(w, storage.GetMemStorage(ctx).Gauge)
-		fmt.Println(storage)
 	}
+}
+
+func RetrieveOneMHandle(storage repositories.Repo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		mType := chi.URLParam(r, "mType")
+		mName := chi.URLParam(r, "mName")
+		ctx := context.Background()
+		if mType == "counter" {
+			counter := storage.GetMemStorage(ctx).Counter
+			valueNum, ok := counter[mName]
+			if !ok {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", valueNum)))
+			return
+		}
+		if mType == "gauge" {
+			gauge := storage.GetMemStorage(ctx).Gauge
+			valueNum, ok := gauge[mName]
+			if !ok {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			valueStr := fmt.Sprintf("%g", valueNum)
+			w.Write([]byte(valueStr))
+			return
+		}
+	}
+}
+
+func BadRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	return
 }
