d2a6d4b0ff6e7e74a3006cb818329eba97d1ef2c
netzen86 <netzen0su@gmail.com>
2024-09-03 23:14:29 +0500

    restore default true

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -37,7 +37,7 @@ func main() {
 	flag.StringVar(&endpoint, "a", addressServer, "Used to set the address and port on which the server runs.")
 	flag.StringVar(&fileStoragePath, "f", saveMetricsDefaultPath, "Used to set file path to save metrics.")
 	flag.StringVar(&dbconstring, "d", "", "Used to set file path to save metrics.")
-	flag.BoolVar(&restore, "r", false, "Used to set restore metrics.")
+	flag.BoolVar(&restore, "r", true, "Used to set restore metrics.")
 	flag.IntVar(&storeInterval, "i", storeIntervalDef, "Used for set save metrics on disk.")
 
 	flag.Parse()
