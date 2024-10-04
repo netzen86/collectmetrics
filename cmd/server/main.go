b72ef8e2752984ddc8cb953d1924504390aefb8b
netzen86 <netzen0su@gmail.com>
2024-10-04 20:59:02 +0500

    added package router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/netzen86/collectmetrics/config"
-	"github.com/netzen86/collectmetrics/internal/handlers"
 	"github.com/netzen86/collectmetrics/internal/repositories/files"
+	"github.com/netzen86/collectmetrics/internal/router"
 )
 
 func main() {
@@ -19,34 +17,7 @@ func main() {
 		log.Fatalf("error when getting config %v ", err)
 	}
 
-	gw := chi.NewRouter()
-
-	gw.Route("/", func(gw chi.Router) {
-		gw.Post("/", handlers.WithLogging(handlers.BadRequest))
-		gw.Post("/update/", handlers.WithLogging(handlers.JSONUpdateMMHandle(
-			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
-			cfg.DBconstring, cfg.StorageSelecter, cfg.SignKeyString, cfg.StoreInterval)))
-		gw.Post("/updates/", handlers.WithLogging(handlers.JSONUpdateMMHandle(
-			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
-			cfg.DBconstring, cfg.StorageSelecter, cfg.SignKeyString, cfg.StoreInterval)))
-		gw.Post("/value/", handlers.WithLogging(handlers.JSONRetrieveOneHandle(
-			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.DBconstring,
-			cfg.StorageSelecter, cfg.SignKeyString)))
-		gw.Post("/update/{mType}/{mName}", handlers.WithLogging(handlers.BadRequest))
-		gw.Post("/update/{mType}/{mName}/", handlers.WithLogging(handlers.BadRequest))
-		gw.Post("/update/{mType}/{mName}/{mValue}", handlers.WithLogging(handlers.UpdateMHandle(
-			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
-			cfg.DBconstring, cfg.StorageSelecter)))
-		gw.Post("/*", handlers.WithLogging(handlers.NotFound))
-
-		gw.Get("/ping", handlers.WithLogging(handlers.PingDB(cfg.DBconstring)))
-		gw.Get("/value/{mType}/{mName}", handlers.WithLogging(handlers.RetrieveOneMHandle(
-			cfg.MemStorage, cfg.FileStoragePath, cfg.DBconstring, cfg.StorageSelecter)))
-		gw.Get("/", handlers.WithLogging(handlers.RetrieveMHandle(cfg.MemStorage)))
-		gw.Get("/*", handlers.WithLogging(handlers.NotFound))
-
-	},
-	)
+	gw := router.GetGateway(cfg)
 
 	if cfg.StoreInterval != 0 {
 		go files.SaveMetrics(cfg.MemStorage, cfg.FileStoragePathDef,
---
+++ internal/router/router.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"fmt"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/netzen86/collectmetrics/config"
+	"github.com/netzen86/collectmetrics/internal/handlers"
+)
+
+func GetGateway(cfg config.ServerCfg) chi.Router {
+
+	gw := chi.NewRouter()
+
+	gw.Route("/", func(gw chi.Router) {
+		gw.Post("/", handlers.WithLogging(handlers.BadRequest))
+		gw.Post("/update/", handlers.WithLogging(handlers.JSONUpdateMMHandle(
+			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
+			cfg.DBconstring, cfg.StorageSelecter, cfg.SignKeyString, cfg.StoreInterval)))
+		gw.Post("/updates/", handlers.WithLogging(handlers.JSONUpdateMMHandle(
+			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
+			cfg.DBconstring, cfg.StorageSelecter, cfg.SignKeyString, cfg.StoreInterval)))
+		gw.Post("/value/", handlers.WithLogging(handlers.JSONRetrieveOneHandle(
+			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.DBconstring,
+			cfg.StorageSelecter, cfg.SignKeyString)))
+		gw.Post("/update/{mType}/{mName}", handlers.WithLogging(handlers.BadRequest))
+		gw.Post("/update/{mType}/{mName}/", handlers.WithLogging(handlers.BadRequest))
+		gw.Post("/update/{mType}/{mName}/{mValue}", handlers.WithLogging(handlers.UpdateMHandle(
+			cfg.MemStorage, fmt.Sprintf("%stmp", cfg.FileStoragePath), cfg.FileStoragePathDef,
+			cfg.DBconstring, cfg.StorageSelecter)))
+		gw.Post("/*", handlers.WithLogging(handlers.NotFound))
+
+		gw.Get("/ping", handlers.WithLogging(handlers.PingDB(cfg.DBconstring)))
+		gw.Get("/value/{mType}/{mName}", handlers.WithLogging(handlers.RetrieveOneMHandle(
+			cfg.MemStorage, cfg.FileStoragePath, cfg.DBconstring, cfg.StorageSelecter)))
+		gw.Get("/", handlers.WithLogging(handlers.RetrieveMHandle(cfg.MemStorage)))
+		gw.Get("/*", handlers.WithLogging(handlers.NotFound))
+
+	},
+	)
+	return gw
+}
