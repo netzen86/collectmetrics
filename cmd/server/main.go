3b704e1e4bfb542e8568531b96638e077f50c04d
netzen86 <netzen0su@gmail.com>
2024-10-14 23:51:48 +0500

    added func migration and restore metric from file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"github.com/netzen86/collectmetrics/config"
+	"github.com/netzen86/collectmetrics/internal/repositories/db"
 	"github.com/netzen86/collectmetrics/internal/repositories/files"
 	"github.com/netzen86/collectmetrics/internal/router"
+	"github.com/netzen86/collectmetrics/internal/server"
 )
 
 func main() {
 	var cfg config.ServerCfg
+	ctx := context.Background()
 
 	log.Println("!!! SERVER START !!!")
 
@@ -20,10 +24,27 @@ func main() {
 		log.Fatalf("error when getting config %v ", err)
 	}
 
+	// если хранилище база данных то создаем необходимые таблицы
+	_, dbstor := cfg.Storage.(*db.DBStorage)
+	if dbstor {
+		err = server.MakeDBMigrations(ctx, cfg)
+		if err != nil {
+			log.Fatalf("error when making migration %v ", err)
+		}
+	}
+
+	// восстанавливаем метрики из файла
+	if cfg.Restore {
+		err := server.RestoreM(ctx, cfg)
+		if err != nil {
+			log.Fatalf("error when restoring metircs from file %v ", err)
+		}
+	}
+
 	// получаем роутер
 	gw := router.GetGateway(cfg)
 
-	// восстанавливаем метрики из файла
+	// сохраняем метрики из файл
 	if cfg.StoreInterval != 0 {
 		go files.SaveMetrics(cfg.Storage, cfg.FileStoragePathDef,
 			cfg.StorageSelecter, cfg.StoreInterval)
